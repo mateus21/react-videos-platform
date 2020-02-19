2ed769df6db50f4e61f8a5bcb643bdec8f51c9e7
Mateus Mantovani <mateus21mantovani@hotmail.com>
2020-02-18 22:46:01 -0300

    Add BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            PageOne
+            <Link to='/pagetwo'>Navigate to Page Two</Link>
+        </div>);
+};
+
+const PageTwo = () => {
+    return <div>
+        PageTwo
+        <button>Teste</button>
+        <Link to='/'>Navigate to Page One</Link>
+    </div>;
+};
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <Route path="/" exact component={PageOne} />
+                <Route path='/pagetwo' component={PageTwo} />
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
