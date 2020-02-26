f6257d03521292835ee92cb1d23cf9c3883b9493
Mateus Mantovani <mateus21mantovani@hotmail.com>
2020-02-26 00:26:58 -0300

    Create Modal with React Portal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
-    return <div>StreamDelete</div>
+    const actions = (
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    );
+
+    return (
+        <div>
+            Stream Delete
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this Stream?"
+                actions={actions}
+            />
+        </div>
+    );
 };
 
 export default StreamDelete;
